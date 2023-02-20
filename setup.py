42e0c12786b8576773dcd25ad44531c4202bd0c2
HichemELB <hichem@sendinblue.com>
2023-02-20 17:39:09 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 # google-auth-httplib2, google-api-python-client, and pytz libraries.
 # They will be installed automatically when the script is executed with the --setup_file option.
 REQUIRED_PACKAGES = [
-    'apache-beam[gcp]==2.30.0',
-    'google-cloud-bigquery==2.26.0',
-    'google-cloud-storage==1.38.0',
-    'google-auth==1.35.0',
-    'google-auth-oauthlib==0.4.5',
-    'google-auth-httplib2==0.4.6',
-    'google-api-python-client==2.30.0',
-    'pytz==2021.3',
+    'apache-beam[gcp]==2.45.0',
+    'google-cloud-bigquery==3.5.0',
+    'google-cloud-storage==2.7.0',
+    'google-auth==2.16.1',
+    'google-auth-oauthlib==1.0.0',
+    'google-auth-httplib2==0.1.0',
+    'google-api-python-client==2.78.0',
+    'pytz==2022.7.1',
     'fire==0.5.0',
     'pytest==7.2.1'
 ]
